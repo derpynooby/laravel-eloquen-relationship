791f1ca5d205af741f33261919b7c769ffc97510
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-10 11:15:50 +0700

    Adding index method to post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * index
+     *
+     * 
+     */
+    public function index()
+    {
+        //get all posts from Model
+        $posts = Post::latest()->get();
+
+        //passing posts to view
+        return view('posts', compact('posts'));
+    }
 }
