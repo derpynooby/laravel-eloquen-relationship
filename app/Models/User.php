70151fdc2050bffb0644d4dc742fb41956be8d04
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-10 11:48:41 +0700

    Adding role relationship to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -55,5 +56,15 @@ class User extends Authenticatable
     public function phone(): HasOne
     {
     	return $this->hasOne(Phone::class);
+    }
+
+    /**
+     * roles
+     *
+     * @return void
+     */
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'user_role');
     }
 }
