797764d14774384b294c79edd923e3d8dec4e84a
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-10 10:25:00 +0700

    Adding user relationship to phone model

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Phone extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * user
+     *
+     * @return void
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
