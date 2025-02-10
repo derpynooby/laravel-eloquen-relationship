40c299beb2c507cd077a93a6e6746491b04d1bda
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-10 10:28:11 +0700

    Adding index method to user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+     /**
+     * index
+     *
+     * 
+     */
+    public function index()
+    {
+        //get users form Model
+        $users = User::latest()->get();
+
+         //passing user to view
+         return view('users', compact('users'));
+    }
 }
