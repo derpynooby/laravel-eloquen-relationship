a1ad48b22203652e73c517d19564144d8dd02859
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-10 10:20:27 +0700

    Editing phone migration

--- database/migrations/2025_02_10_031813_create_phones_table.php
+++ database/migrations/2025_02_10_031813_create_phones_table.php
@@ -9,13 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('phones', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('phones', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('user_id');
+        $table->string('phone');
+        $table->timestamps();
+
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+    });
+}
 
     /**
      * Reverse the migrations.
