9ae16f65aaadb1d7997227682dfd595ea919edab
derpynooby <149212764+derpynooby@users.noreply.github.com>
2025-02-10 11:12:56 +0700

    Adding comment relationship to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+    
+    /**
+     * comments
+     *
+     * @return void
+     */
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
